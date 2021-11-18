5c9e2d8c5bfefad679ea4c5acfd4a974ae678f99
Anas Bajjouk <bajjouk1@gmail.com>
2021-11-18 22:41:12 +0100

    file structure + some css

---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./possibility.css"
+
+const Possibility = () => {
+    return (
+        <div>
+            Possibility.jsx
+        </div>
+    )
+}
+
+export default Possibility
